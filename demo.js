020480668969c3a974f4e9c7054ff2f72efb33ee
Sui Gn <natsynapses@gmail.com>
2023-08-20 00:55:10 -0400

    1.4.2

--- demo.js
+++ demo.js
@@ -1,10 +1,7 @@
 const Atom = require('./atom.js');
 const atomInstance = new Atom();
-const electron1 = atomInstance.createElectron('./atom-ui.html');
-electron1.send('some-message', { data: 'Hello, Electron 1!' });
-
-const electron2 = atomInstance.createElectron('./atom-ui.html');
-electron2.send('another-message', { data: 'Hello, Electron 2!' });
-
-// This will start the WebSocket server on port 8080
 atomInstance.wsServerOn();
+console.log('Atom instance created with id:', atomInstance.id);
+console.log('Creating Electron...:');
+const electron1 = atomInstance.createElectron('./atom-ui.html');
+// This will start the WebSocket server on port 8080
--- electron.js
+++ electron.js
@@ -4,20 +4,20 @@ const { ipcMain } = require('electron');   // Add this line for IPC
 const path = require('path');
 class Electron {
   constructor(content, atomId) {
-    this.id = atomId; 
+    this.atomId = atomId; 
     this.setupProcess(content);
   }
   setupProcess(content) {
     const electronMainPath = path.join(__dirname, 'electronMain.js');
     this.process = spawn(electronPath, [electronMainPath, content]);
     this.process.stdout.on('data', (data) => {
-      console.log(`Electron ${this.id} stdout: ${data}`);
+      console.log(`Electron of atom:  ${this.atomId} stdout: ${data}`);
     });
     this.process.stderr.on('data', (data) => {
-      console.error(`Electron ${this.id} stderr: ${data}`);
+      console.error(`Electron of atom: ${this.atomId} stderr: ${data}`);
     });
     this.process.on('close', (code) => {
-      console.log(`Electron process ${this.id} exited with code ${code}`);
+      console.log(`Electron process of atom:  ${this.atomId} exited with code ${code}`);
     });
   }
 
